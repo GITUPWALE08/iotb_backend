16b1520498b3b4ecb7028292c6a4c9bc39bac659
eastcoast <princeadewale391@gmail.com>
2026-01-19 23:42:08 +0100

    added super user script

--- core/settings.py
+++ core/settings.py
@@ -32,12 +32,14 @@ DEBUG = 'RENDER'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST = 'smtp.gmail.com' #os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
-EMAIL_USE_TLS = True  # Brevo uses TLS for port 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False # Do not use SSL on port 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'princeadewale391@gmail.com')
+EMAIL_HOST_PASSWORD = 'pcff ntmw allr vtgh' #os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_TIMEOUT = 30
 
 
 ALLOWED_HOSTS = [
@@ -228,10 +230,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# For Real Production (Example with Gmail/SMTP):
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.sendgrid.net'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'apikey'
-# EMAIL_HOST_PASSWORD = 'YOUR_SENDGRID_API_KEY'
