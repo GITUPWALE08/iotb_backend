38e512b047fdd7598591ca55900b3d5449092f06
GITUPWALE08 <princeadewale391@gmail.com>
2026-04-07 15:40:30 +0100

    gt

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
 DEBUG = not os.environ.get("RENDER")
 
 ALLOWED_HOSTS = [
-    ".onrender.com",
+    "https://iot-bridge.onrender.com/",
     "localhost",
     "127.0.0.1",
 ]
